f60784b442f5ff4e353ad22ccbc49686f67ba04a
Ahahabuto <ahahabuto@yandex.ru>
2025-04-06 22:15:55 +0300

    Sprint_6

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,41 @@
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FelineTest {
+
+    private static final String EXPECTED_FAMILY = "Кошачьи";
+    private static final int EXPECTED_KITTENS_COUNT = 1;
+
+    @Spy
+    private Feline feline;
+
+    @Test
+    public void eatMeatTest() throws Exception{
+        feline.eatMeat();
+        verify(feline, times(1)).getFood("Хищник");
+    }
+
+    @Test
+    public void getFamilyTest() {
+        assertEquals(EXPECTED_FAMILY, feline.getFamily());
+    }
+
+    @Test
+    public void getKittensTest() {
+        assertEquals(EXPECTED_KITTENS_COUNT, feline.getKittens());
+    }
+
+    @Test
+    public void getKittensWithCountTest() {
+        int kittensCount = 10;
+        assertEquals(kittensCount, feline.getKittens(kittensCount));
+    }
+}
