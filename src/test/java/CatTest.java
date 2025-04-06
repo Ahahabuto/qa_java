264aa904bac585bda03147aae417de09f6b6b974
Ahahabuto <ahahabuto@yandex.ru>
2025-04-06 21:52:05 +0300

    Sprint_6

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,35 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    private Cat cat;
+
+    @Mock
+    private Feline feline;
+
+    @Before
+    public void setUp() {
+        cat = new Cat(feline);
+    }
+
+    @Test
+    public void getSoundTest() {
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        cat.getFood();
+        verify(feline).eatMeat();
+    }
+}
---
+++ src/test/java/LionTest.java
@@ -0,0 +1,53 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    private static final String MALE = "Самец";
+    private static final String TEXT_OF_EXCEPTION = "Используйте допустимые значения пола животного - самец или самка";
+    private static final List<String> FOOD = List.of("Животные", "Птицы", "Рыба");
+    private static final String UNSUPPORTED_SEX = "Неподдерживаемый пол";
+
+    @Mock
+    private Feline feline;
+    private Lion lion;
+
+    @Before
+    public void setUp() throws Exception {
+        lion = new Lion(feline, MALE);
+    }
+
+    @Test
+    public void getKittensTest() {
+        int kittensCount = 7;
+        when(feline.getKittens()).thenReturn(kittensCount);
+        assertThat(lion.getKittens()).isEqualTo(kittensCount);
+        verify(feline).getKittens();
+    }
+
+    @Test
+    public void doesHaveManeExceptionTest() {
+        Throwable throwable = catchThrowable(() -> new Lion(feline, UNSUPPORTED_SEX));
+        assertThat(throwable).isInstanceOf(Exception.class).hasMessage(TEXT_OF_EXCEPTION);
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        when(feline.getFood("Хищник")).thenReturn(FOOD);
+        assertThat(lion.getFood()).isEqualTo(FOOD);
+        verify(feline).getFood("Хищник");
+    }
+}
